922fd09e291324d182e0a488ddbd808097ee0d39
Daniel Falbel <dfalbel@gmail.com>
2023-09-27 15:37:54 -0300

    a few slides updates and code updates

--- examples/01-basics.R
+++ examples/01-basics.R
@@ -5,25 +5,32 @@ a <- array(runif(10000), dim = c(100, 10, 10))
 
 # create torch tensors from R objects
 
-t_m <- torch_tensor(m)
+t_m <- torch_tensor(m, dtype = "double")
 t_a <- torch_tensor(a)
 
 # back to R
 
 as.array(t_m)
+as.matrix(t_m)
 
 # attributes of a tensor
 
 t_m$device
 t_m$dtype
-t_m$shape
+t_m$shape # dim(t_m)
 t_m$requires_grad
 
 # methods
 
-t_m$abs()
+(t_m -1)$abs()
+t_m$add(1)
 t_m$sum()
 t_m$min()
+
+# in-place methods end in `_`
+
+t_m$add_(1)
+t_m # t_m is modified in place, no copy is made
 
 # built-in functions
 
@@ -31,15 +38,11 @@ torch_mm(t_m, t_m$t())
 torch_sum(t_m)
 torch_mean(t_m)
 
-# in-place methods end in `_`
-
-t_m$add_(1)
-t_m # t_m is modified in place, no copy is made
-
 # changing the data type
 
 t_m$to(dtype = "float64")
 t_m$to(dtype = torch_float64())
+# torch_tensor(m, dtype = "float64")
 
 # tensor computations happen on it's current device
 
@@ -47,8 +50,9 @@ mps_tensor <- t_m$to(device="mps") # only available on ARM Macs
 # on GPU's try instead : t_m$to(device="cuda") 
 
 out <- torch_mm(mps_tensor, mps_tensor$t())
+out$to(device="cpu")
 
-cpu_tensor <- torch_randn(1000, 1000)
+cpu_tensor <- torch_randn(10000, 10000)
 mps_tensor <- cpu_tensor$to(device="mps")
 
 bench::mark(

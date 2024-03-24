21e8fdc72c7d0ff35053342c23ade9f9bf883983
Howard Baek <50791792+howardbaek@users.noreply.github.com>
2024-03-24 15:50:48 -0700

    Add my notes to 01-basics.R

--- examples/01-basics.R
+++ examples/01-basics.R
@@ -1,53 +1,57 @@
 library(torch)
 
+# A matrix with 100 rows and 10 columns
 m <- matrix(runif(1000), nrow = 100)
+# An array with 3 dimensions
 a <- array(runif(10000), dim = c(100, 10, 10))
 
-# create torch tensors from R objects
-
-t_m <- torch_tensor(m, dtype = "double")
+# Create torch tensors from R objects
+t_m <- torch_tensor(m, dtype = "double") # CPUDoubleType{100, 10}: A tensor that lives on the CPU and a floating type tensor with dimensions [100, 10]
 t_a <- torch_tensor(a)
+# In general, use 'dtype = "float"' unless you need double precision!
 
-# back to R
-
+# Convert torch tensors back to R
 as.array(t_m)
 as.matrix(t_m)
 
-# attributes of a tensor
+# Attributes of a tensor
+t_m$device # 'device' : the location where your tensor is stored (e.g. CPU or GPU)
+# and device (GPU/CPU) is where the computation is happening. 
+t_m$dtype # 'dtype' : data type of the tensor
+t_m$shape # 'shape' : equivalent to dim(t_m) in R
+t_m$requires_grad # 'requires_grad' : related to autograd system
 
-t_m$device
-t_m$dtype
-t_m$shape # dim(t_m)
-t_m$requires_grad
-
-# methods
-
+# Methods: torch tensors are R6 objects in R and these methods are functions 
+# that can access the tensor. They are all mathematical operations you can do arrays.
 (t_m -1)$abs()
 t_m$add(1)
 t_m$sum()
 t_m$min()
 
-# in-place methods end in `_`
-
+# in-place methods end in `_`: modify the tensor in-place
 t_m$add_(1)
 t_m # t_m is modified in place, no copy is made
+# This exists mostly for performance reasons (to avoid expensive operations)
 
-# built-in functions
-
+# Built-in functions (similar to 'sum()', 'mean()' in R)
 torch_mm(t_m, t_m$t())
 torch_sum(t_m)
 torch_mean(t_m)
+# Usually, there is a 1:1 mapping between methods and built-in functions.
+# Sometimes, there won't be a 1:1 correspondence.
 
-# changing the data type
-
+# Changing the data type
 t_m$to(dtype = "float64")
 t_m$to(dtype = torch_float64())
 # torch_tensor(m, dtype = "float64")
 
-# tensor computations happen on it's current device
 
+# tensor computations happen on it's current device
 mps_tensor <- t_m$to(device="mps") # only available on ARM Macs
-# on GPU's try instead : t_m$to(device="cuda") 
+# on NVIDIA GPU's try instead : t_m$to(device="cuda") 
+# This will use NVIDIA's CUDA library to run computations on the tensor
+
+# 'cuda_is_available()' is the function that tells you if CUDA is available
 
 out <- torch_mm(mps_tensor, mps_tensor$t())
 out$to(device="cpu")
@@ -59,4 +63,3 @@ bench::mark(
   mps = torch_mm(mps_tensor, mps_tensor$t()),
   cpu = torch_mm(cpu_tensor, cpu_tensor$t())
 )
-

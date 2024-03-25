c1046c429a793e8a001839e420f214735ba95a99
Howard Baek <50791792+howardbaek@users.noreply.github.com>
2024-03-25 14:55:54 -0700

    Notes for 03-nn-optim.R

--- examples/03-nn-optim.R
+++ examples/03-nn-optim.R
@@ -1,16 +1,26 @@
 library(torch)
+library(dplyr)
 
 # if we'd like to modularize our code using just R tools, we probably would
 # do something like this;
 
-linear_model <- function(x, state) {
-  x$mm(state$W) + state$b
-}
-
-state <- list(
-  W = torch_randn(10, 1, requires_grad = TRUE),
-  b = torch_zeros(1, 1, requires_grad = TRUE)
+linear <- nn_module(
+  "linear",
+  initialize = function() {
+    self$W <- nn_parameter(torch_randn(10, 1))
+    self$b <- nn_parameter(torch_zeros(1, 1))
+  },
+  forward = function(x) {
+    x$mm(self$W) + self$b
+  }
 )
+
+model <- linear() # essentially calling the initialize() function inside linear
+# model() is a function that calls forward()
+model(torch_randn(10, 10))
+
+# The optimizer is just an R object with a consistent API
+opt <- optim_sgd(model$parameters, lr = 0.001)
 
 # train on mtcars ------
 
@@ -26,19 +36,15 @@ y_t <- torch_tensor(y)
 for (i in 1:50000) {
   
   # we use a function here
-  y_hat <- linear_model(x_t, state)
+  y_hat <- model(x_t)
   
   loss <- torch_mean((y_t$view(c(-1, 1)) - y_hat)^2)
   
   loss$backward()
   
   # we can gradient updates in a for loop
-  with_no_grad({
-    for(param in state) {
-      param$sub_(lr * param$grad)
-      param$grad$zero_()
-    }
-  })
+  opt$step()
+  opt$zero_grad()
   
   if (i %% 1000 == 0)
     cat("Loss ", loss$item(), "\n")
